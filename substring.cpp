2a3cd8c3dfa6f1b78fdb38a189e8e57031cb5114
USER <tamimachy9897@gmail.com>
2024-02-06 09:17:42 +0600

    update check

--- substring.cpp
+++ substring.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 string subString(string text, int start, int len){
     int i;
     string subT = "";
@@ -12,13 +11,11 @@ string subString(string text, int start, int len){
 string addString(string text, int pos, string newT){
     string s1 = subString(text,0,pos);
     string s2 = subString(text,pos,text.size()-s1.size());
-
     return s1+newT+s2;
 }
 string removeString(string text, int pos, int len){
     string s1 = subString(text,0,pos);
     string s2 = subString(text,pos+len,text.size()-(pos+len));
-
     return s1+s2;
 }
 int main(){
