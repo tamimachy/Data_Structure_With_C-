f0b1a6db8d39cca46d0769da1da913786f691b57
USER <tamimachy9897@gmail.com>
2024-02-18 00:05:20 +0600

    insert array

---
+++ array_insert_value.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int insertA(int array[], int n, int pos, int value){
+    n++;
+    int i;
+    for(i=n-1; i>pos; i--){
+        array[i]=array[i-1];
+    }
+    array[pos] = value;
+    return n;
+}
+int main(){
+    int n,i;
+    cout << "Array size: ";
+    cin >> n;
+    int array[n];
+    cout<< "Array elements : ";
+    for(i=0; i<n; i++){
+        cin>> array[i];
+        cout<<"Array values are: "<<array[i] << '\n';
+    }
+    n = insertA(array, n, 3, 25);
+    for(i=0; i<n; i++){
+        cout << "Array values are updated is: " << array[i] << '\n';
+    }
+    return 0;
+}
